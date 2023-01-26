5e7171c0dc4619e8e97a1cf144383a724a623db0
mekhribon08 <muxayyoxoljigitova79@gmail.com>
2023-01-26 22:21:55 +0500

    birthday counter

--- js/main.js
+++ js/main.js
@@ -24,3 +24,17 @@ function countTimer() {
 }
 
 setInterval(countTimer, 1000);
+
+function timeShower() {
+  var timeNow = new Date();
+
+  document.querySelector(".hours").textContent = timeNow.getHours();
+  document.querySelector(".minutes").textContent = timeNow.getMinutes();
+  document.querySelector(".seconds").textContent = timeNow.getSeconds();
+}
+
+timeShower();
+
+setInterval(function () {
+  timeShower();
+}, 1000);
