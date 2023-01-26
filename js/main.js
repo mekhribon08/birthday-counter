25e26545016fd139660af1bd058760e68158d539
mekhribon08 <muxayyoxoljigitova79@gmail.com>
2023-01-26 21:33:12 +0500

    birthday time

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const elDay = document.querySelector("[data-day]");
+const elHour = document.querySelector("[data-hour]");
+const elMinut = document.querySelector("[data-minut]");
+const elSecund = document.querySelector("[data-secund]");
+const elBtn = document.querySelector("[data-btn]");
+const elInputDate = document.querySelector("[data-input-date]");
+const elInputMonth = document.querySelector("[data-input-month]");
+const newDate = new Date();
+
+elBtn.addEventListener("click", (evt) => {
+  countTimer(evt);
+});
+
+function countTimer() {
+  const num = new Date(`${elInputMonth}-${elInputDate} 00:00:00`) - newDate;
+  const days = Math.round(num / (1000 * 60 * 60 * 24));
+  const hours = Math.round(num / (1000 * 60 * 60)) % 24;
+  const minutes = Math.round(num / (1000 * 60)) % 60;
+  const seconds = Math.round(num / 1000) % 60;
+  elDay.textContent = days;
+  elHour.textContent = hours;
+  elMinut.textContent = minutes;
+  elSecund.textContent = seconds;
+}
+
+setInterval(countTimer, 1000);
